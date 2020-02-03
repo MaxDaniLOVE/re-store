23ac691f57f1606c815e19fcb2e03d9b1a5b740e
MaxDaniLOVE <mahseeen@gmail.com>
2020-02-03 10:46:15 +0300

    feat: add build

---
+++ build/precache-manifest.84b1213b0e9ac8109e2882eebec503a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37517d2ae58d1e9bc0fa35fd402c69a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "184ee08bf429f2702b7a",
+    "url": "/static/css/main.bcb47f86.chunk.css"
+  },
+  {
+    "revision": "cec1a4e339c0d72ef138",
+    "url": "/static/js/2.f7f2951b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f7f2951b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "184ee08bf429f2702b7a",
+    "url": "/static/js/main.acca9114.chunk.js"
+  },
+  {
+    "revision": "844d94be027ecd06444f",
+    "url": "/static/js/runtime-main.5a5f5702.js"
+  }
+]);
